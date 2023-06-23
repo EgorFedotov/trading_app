93e0419eba819a1ba0d00db9694ae916d4e50c4f
EgorFedotov <egorfedotovarz@gmail.com>
2023-06-23 11:30:18 +0300

    exceptions

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -13,15 +13,30 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation).where(operation.c.type == operation_type)
-    result = await session.execute(query)
-    return result.all()
+@router.get("")
+async def get_specific_operations(
+        operation_type: str,
+        session: AsyncSession = Depends(get_async_session)):
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+        result = await session.execute(query)
+        return {
+            "status": "success",
+            "data": result.all(),
+            "details": None
+        }
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
 
 
-@router.post("/")
-async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+@router.post("")
+async def add_specific_operations(
+        new_operation: OperationCreate,
+        session: AsyncSession = Depends(get_async_session)):
     stmt = insert(operation).values(**new_operation.dict())
     await session.execute(stmt)
     await session.commit()
